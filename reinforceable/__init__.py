d26f3528e1b8236c0636251e5a081ca78e4a1a93
Alexander Kensert <alexander.kensert@gmail.com>
2023-11-22 15:35:26 +0100

    update __init__.py

--- reinforceable/__init__.py
+++ reinforceable/__init__.py
@@ -2,6 +2,13 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+from reinforceable import envs 
+from reinforceable import agents 
+from reinforceable.agents import ppo 
+from reinforceable import layers 
+from reinforceable import distributions
+from reinforceable import utils
+
 from reinforceable.timestep import Timestep 
 from reinforceable.trajectory import Trajectory
 from reinforceable.buffer import Buffer
